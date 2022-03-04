7cbed375f94cd5e628dfcefb099ed87d696134b9
dsolerh <dsolerh.cinter95@gmail.com>
2022-03-04 00:55:57 -0500

    (add model) booking

---
+++ models/booking.model.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const bookingSchema = new Schema(
+  {
+    event: {
+      type: Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Booking", bookingSchema);
