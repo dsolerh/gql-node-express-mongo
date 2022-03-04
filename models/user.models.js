b20ad1c23da7e8f6237cb29dd03e9c711e515c1b
dsolerh <dsolerh.cinter95@gmail.com>
2022-03-04 00:55:42 -0500

    (add model) user

---
+++ models/user.models.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdEvents: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+  ],
+});
+
+module.exports = model("User", userSchema);
